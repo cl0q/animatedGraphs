044a6e5b3f4ff955d3af0e9f48bc34385f2060e0
Oliver Ilczuk <i23016@hb.dhbw-stuttgart.de>
2024-07-08 17:09:39 +0200

    add looped edges in GraphDrawer

--- src/graph/marking/MarkedVertex.java
+++ src/graph/marking/MarkedVertex.java
@@ -1,12 +1,16 @@
 package graph.marking;
 
+import graph.Edge;
 import graph.Vertex;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarkedVertex<T extends VertexMarking> extends Vertex {
 
     private T marking;
+    private List<Edge> edges = new ArrayList<>();  // List to store edges connected to this vertex
 
     public MarkedVertex() {
         super();
@@ -28,6 +32,18 @@ public class MarkedVertex<T extends VertexMarking> extends Vertex {
 
     public void setMarking(T marking) {
         this.marking = marking;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void addEdge(Edge edge) {
+        edges.add(edge);
+    }
+
+    public void removeEdge(Edge edge) {
+        edges.remove(edge);
     }
 
     public String toString() {
