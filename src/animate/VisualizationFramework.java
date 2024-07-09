7bab3929306b381ed17bd8aa683ddcdfe820c32d
Ilczuk <i23016@hb.dhbw-stuttgart.de>
2024-07-09 17:26:19 +0200

    Fix package structure error caused by merge

--- src/animate/VisualizationFramework.java
+++ src/animate/VisualizationFramework.java
@@ -1,11 +1,9 @@
 package animate;
 
-import animate.drawing.DrawHelper;
-import animate.drawing.GraphDrawer;
-import animate.drawing.TextArea;
+import animate.drawing.*;
 import logging.LogElement;
 import logging.LogElementList;
-import visualization.*;
+import visualization.HybridWindow;
 
 import javax.swing.*;
 
@@ -18,10 +16,11 @@ public class VisualizationFramework {
     public static <T extends logging.Algorithm> void init(T algorithm, animate.drawing.ParameterArea parameterArea, GraphDrawer graphDrawer) {
         LogElementList<LogElement> logList = new LogElementList<>();
         DrawHelper drawHelper = new DrawHelper(graphDrawer);
-        animate.drawing.DrawArea drawArea = new animate.drawing.DrawArea(logList, "visualization", drawHelper, algorithm);
-        animate.drawing.TextArea textArea = new animate.drawing.TextArea(logList);
-        animate.drawing.LegendArea legendArea = new animate.drawing.LegendArea();
-        HybridWindow<animate.drawing.DrawArea, TextArea, animate.drawing.ParameterArea, T, LogElement, animate.drawing.LegendArea> applet = new HybridWindow<>(drawArea, textArea, parameterArea, algorithm, logList, legendArea);
+        DrawArea drawArea = new DrawArea(logList, "visualization", drawHelper, algorithm);
+        TextArea textArea = new TextArea(logList);
+        LegendArea legendArea = new LegendArea();
+        HybridWindow<DrawArea, TextArea, ParameterArea, T, LogElement, LegendArea> applet
+                = new HybridWindow<>(drawArea, textArea, parameterArea, algorithm, logList, legendArea);
 
         JFrame frame = new JFrame("Visualize " + algorithm.getTitle());
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
--- src/animate/algorithm/AlgorithmTopologicalSort.java
+++ src/animate/algorithm/AlgorithmTopologicalSort.java
@@ -1,40 +1,52 @@
 package animate.algorithm;
 
-import animate.drawing.GraphDrawer;
 import animate.drawing.GraphLogElement;
-import graph.structure.graph.DirectedGraph;
 import graph.marking.edge.EdgeColorMarking;
 import graph.marking.vertex.VertexColorMarking;
+import graph.structure.graph.DirectedGraph;
 import logging.LogElementList;
 
+/**
+ * Implementiert einen Algorithmus zur topologischen Sortierung.
+ * Diese Klasse erweitert die Algorithm-Klasse und überschreibt die run-Methode,
+ * um die topologische Sortierung auf einem gerichteten Graphen auszuführen.
+ */
 public class AlgorithmTopologicalSort extends logging.Algorithm {
 
-    final GraphDrawer graphDrawer;
     final DirectedGraph<VertexColorMarking, EdgeColorMarking> directedGraph;
 
+    /**
+     * Konstruktor für den Algorithmus zur topologischen Sortierung.
+     *
+     * @param parameterArea der Parameterbereich für die Visualisierung
+     * @param directedGraph der gerichtete Graph
+     */
     public AlgorithmTopologicalSort(visualization.ParameterArea parameterArea,
-                                    GraphDrawer graphDrawer,
                                     DirectedGraph<VertexColorMarking, EdgeColorMarking> directedGraph) {
         super(parameterArea, "AlgorithmTopologicalSort");
-        this.graphDrawer = graphDrawer;
         this.directedGraph = directedGraph;
     }
 
+    /**
+     * Führt den Algorithmus zur topologischen Sortierung aus.
+     *
+     * @return LogElementList<?> die Liste der Protokollelemente, die während der Ausführung erzeugt wurden
+     */
     @Override
     public LogElementList<?> run() {
         LogElementList<?> logList;
 
         directedGraph.topSort();
-
         logList = directedGraph.getGraphLogElementList();
-
-        directedGraph.getGraphLogElementList().add(new GraphLogElement<>(9999,
-                "[" + title + " done!] : " + directedGraph.workingOrderArrayToString(),
-                0,
-                ((GraphLogElement<VertexColorMarking, EdgeColorMarking>)logList
-                        .getLast())
-                        .getGraph()
-                        .clone()));
+        directedGraph.getGraphLogElementList()    // Protokollelement für den Abschluss des Algorithmus
+                .add(new GraphLogElement<>(9999,
+                        "[" + title + " abgeschlossen!] : " + directedGraph.workingOrderArrayToString(),
+                        0,
+                        ((GraphLogElement<VertexColorMarking, EdgeColorMarking>) logList
+                                .getLast())
+                                .getGraph()
+                                .clone()));
         return logList;
     }
 }
+
